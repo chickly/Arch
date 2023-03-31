06e4508ef9e1c995ed7518abc6eaef92e26d7356
chickly <2893784448@qq.com>
2023-03-31 23:26:42 +0800

    main

--- src/LearnCpp/2.C++基础-函数和文件/2.13如何设计第一个程序/main.cpp
+++ src/LearnCpp/2.C++基础-函数和文件/2.13如何设计第一个程序/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
-
-int main(){
+int getUserInput() {
+  std::cout << "Enter an integer ";
+  int input{};
+  std::cin >> input;
+  return input;
+}
+char getMathematicalOperation() {
   
+}
+int main() {
+  // Get first number from user
+  //    getUserInput();
+  int firstNum{getUserInput()};
+
+  // Get mathematical operation from user
+  //    getMathematicaration();
+
+  // Get second number from user
+  //    getUserInput();
+
+  // Calculate result
+  //    calculateResult();
+
+  // Print result
+  //    printResult();
 
   return 0;
 }
