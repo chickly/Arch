1d4ae1896a9a287343cd57c6ad25376ab7ec508f
chickly <2893784448@qq.com>
2023-05-10 20:48:02 +0800

    master

--- src/LearnCpp/11.数组，字符串和动态内存分配/11.1/test.cpp
+++ src/LearnCpp/11.数组，字符串和动态内存分配/11.1/test.cpp
@@ -4,20 +4,20 @@
 constexpr int Ninety = 90;
 namespace StudentNames {
 enum StudentNames {
-    kenny,       // 0
-    kyle,        // 1
-    stan,        // 2
-    butters,     // 3
-    cartman,     // 4
-    wendy,       // 5
-    GONGYUE,     // 6
-    max_students // 7
+	kenny,       // 0
+	kyle,        // 1
+	stan,        // 2
+	butters,     // 3
+	cartman,     // 4
+	wendy,       // 5
+	GONGYUE,     // 6
+	max_students // 7
 };
 } // namespace StudentNames
 int main() {
-    std::array<int, StudentNames::max_students> Scores{};
-    Scores[StudentNames::StudentNames::stan] = Ninety;
-    std::cout << Scores[2] << '\n';
-    std::cout << "The array has: " << std::size(Scores) << " elements\n";
-    return 0;
+	std::array<int, StudentNames::max_students> Scores{};
+	Scores[StudentNames::StudentNames::stan] = Ninety;
+	std::cout << Scores[2] << '\n';
+	std::cout << "The array has: " << std::size(Scores) << " elements\n";
+	return 0;
 } // namespace StudentNamesenum StudentNames
